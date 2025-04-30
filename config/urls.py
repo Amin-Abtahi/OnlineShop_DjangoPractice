efab2ba94409d18a082788f5f9eb69baaaf4bb56
AMIN <aminsmaa77@gmail.com>
2025-04-30 21:55:49 +0330

    add detail view and modify admin panel

--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+import products
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('accounts/', include('allauth.urls')),
-
-
+    path('products/', include('products.urls')),
 
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Product
 
-# Create your views here.
+class ProductListView(generic.ListView):
+    queryset = Product.objects.filter(active=True)
+    # model = Product
+    template_name = 'products/product_list.html'
+    context_object_name = 'products'
