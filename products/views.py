6954e8a1ed2d6500b89fa0961e6abf1cd8b6f269
AMIN <aminsmaa77@gmail.com>
2025-04-30 22:42:15 +0330

    modify product detail view and models.py

--- products/views.py
+++ products/views.py
@@ -1,8 +1,14 @@
 from django.views import generic
 from .models import Product
 
-class ProductListView(generic.ListView):
+class ProductListView(generic.ListView):  # class base view: CBV
     queryset = Product.objects.filter(active=True)
     # model = Product
     template_name = 'products/product_list.html'
     context_object_name = 'products'
+
+
+class ProductDetailView(generic.DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
