b56281ac534e5ba48042dbffd6a81bf90237a004
AMIN <aminsmaa77@gmail.com>
2025-05-06 00:53:11 +0330

    add form.py and CommentForm class to product app

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
 from django.views import generic
+
+from .forms import CommentForm
 from .models import Product
 
 class ProductListView(generic.ListView):  # class base view: CBV
@@ -12,3 +14,8 @@ class ProductDetailView(generic.DetailView):
     model = Product
     template_name = 'products/product_detail.html'
     context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+        return context
